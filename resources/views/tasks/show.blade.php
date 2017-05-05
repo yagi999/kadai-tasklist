80aa2160bc5ded2f339b730f4ee323b075052e7d
tama <tama@tamappage.com>
2017-05-05 03:46:03 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <p>{{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id]) !!}
+    
+    {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
