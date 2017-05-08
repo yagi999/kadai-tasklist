b82fb43497f582050e9413dd742786fbf75223ff
tama <tama@tamappage.com>
2017-05-08 06:50:59 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,19 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            
     <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+   
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('status', '状態:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        {!! Form::label('status', '状態:') !!}
-        {!! Form::text('status') !!}
-
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,19 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            
+    <h1>id={{ $task->id }} のタスク編集ページ</h1>
 
-    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-        {!! Form::label('status', '状態:') !!}
-        {!! Form::text('status') !!}
-
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('status', '状態:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
