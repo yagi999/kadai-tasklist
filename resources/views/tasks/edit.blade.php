5b28e239e71515ac2de1299de7b024c1eae9279c
tama <tama@tamappage.com>
2017-05-08 09:12:52 +0000

    shusei

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
             
     <h1>id={{ $task->id }} のタスク編集ページ</h1>
-
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
