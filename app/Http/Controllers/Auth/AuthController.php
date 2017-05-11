4efc876f8ef7b39619c4048e65864982b0169445
tama <tama@tamappage.com>
2017-05-11 13:09:26 +0000

    user registration

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,6 +22,8 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+     // 追加
+    protected $redirectTo = '/';
 
     /**
      * Create a new authentication controller instance.
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,6 @@ Route::get('/', function () {
 });
 
 Route::resource('tasks', 'TasksController');
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
